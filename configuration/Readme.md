652eb6a97838e93fdc87e66d7f95619426d766b0
Pascal Bachor <bachorp@informatik.uni-freiburg.de>
2020-12-14 22:43:34 +0100

    Cleanup: Configuration

--- configuration/Readme.md
+++ configuration/Readme.md
@@ -1,5 +1,10 @@
-Requires Pandas and Matplotlib.
+This directory contains a set of test data used to evaluate possible optimizations.
+We generated a single instance per parameter set (4 ≤ g ≤ 12, b ∈ {10, 20}, 1 ≤ a, c ≤ 10) per configuration (with and without preprocessing, f ∈ {1.5, 2}).
+
+You can generate the plots and diagram values from the associated subsection of the paper as follows.
+Additionally, we evaluate the effect of different encodings of the SAT formula.
 
 ```shell
 python3 evaluate.py
 ```
+Requires Pandas, Matplotlib and Scipy.
--- configuration/evaluate.py
+++ configuration/evaluate.py
@@ -6,7 +6,6 @@ import scipy.stats
 import itertools
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-#import seaborn as sns
 
 def plot(xlabel: str, ylabel: str, filename: str, legend=True):
     xpoints = ypoints = plt.xlim()
@@ -16,8 +15,6 @@ def plot(xlabel: str, ylabel: str, filename: str, legend=True):
     axes.xaxis.grid(True, zorder=0)
     plt.plot([1e3, 1e3], [0, 1e3], linestyle='-', color='salmon', zorder=2, lw=1)
     plt.plot([0, 1e3], [1e3, 1e3], linestyle='-', color='salmon', zorder=2, lw=1)
-    # plt.gca().yaxis.grid(True)
-    # plt.gca().xaxis.grid(True)
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel(xlabel)
@@ -94,7 +91,6 @@ def do(data: dict, cf1, cf2, label=None):
     x = []
     y = []
 
-    fs = []
     logs = []
 
     s1 = 0
@@ -109,57 +105,45 @@ def do(data: dict, cf1, cf2, label=None):
         if not pd.isna(v2):
             s2 += 1
 
-        if any(map(lambda x: r[x][0] < 5e2, configurations)):
-            continue
-
         if (pd.isna(v1)):
             v1 = 1e6
         if (pd.isna(v2)):
             v2 = 1e6
+
+        logs.append(math.log2(v2) - math.log2(v1))
+
+        if any(map(lambda x: r[x][0] < 5e2, configurations)):
+            continue
+
         x.append(v1 / 1e3)
         y.append(v2 / 1e3)
 
-        if any(map(lambda x: pd.isna(r[x][0]), configurations)):
-            continue
-
-        fs.append(v2 / v1)
-        logs.append(math.log2(v2) - math.log2(v1))
-
     plt.plot(x, y, next(cyc), label=label, markersize=2.5, zorder=5)
-    #plt.plot(fs, [0] * len(fs), 'x')
-    #sns.kdeplot(fs)
-    print(sum(logs) / len(logs))
 
-    return sum(fs) / len(fs), len(fs), s1, s2
+    return sum(logs), s1, s2, mwu(data, cf1, cf2)
 
 def coverage(data: dict):
     total = 0
     solved = 0
+    always = 0
     for _, r in data.items():
         total += 1
         if any(map(lambda x: not pd.isna(r[x][0]), configurations)):
             solved += 1
-    print("{} out of {} covered".format(solved, total))
+        if all(map(lambda x: not pd.isna(r[x][0]), configurations)):
+            always += 1
+    print("{} out of {} covered. {} solved under every configuration.".format(solved, total, always))
 
 def prepare(data: dict, cf):
     result = []
     for i, r in data.items():
-        if any(map(lambda x: r[x][0] < 5e2, configurations)):
-            pass#continue
         result.append(1e6 if pd.isna(r[cf][0]) else r[cf][0])
     return result
 
-def ranksums(data: dict, cf1, cf2):
+def mwu(data: dict, cf1, cf2):
     vs1 = prepare(data, cf1)
     vs2 = prepare(data, cf2)
-    sign = 0
-    for i, x in enumerate(vs1):
-        if x > vs2[i]:
-            sign += 1
-        elif x < vs2[i]:
-            sign -= 1
-    print(sign)
-    print(scipy.stats.mannwhitneyu(vs1,vs2,alternative='greater'))
+    return scipy.stats.mannwhitneyu(vs2, vs1, alternative='greater').pvalue
 
 if __name__ == '__main__':
 
@@ -182,51 +166,45 @@ if __name__ == '__main__':
 
     configurations = list(map(lambda x: x + tail, ['0|2', '1|2', '0|1.5', '1|1.5']))
 
-    ranksums(result, configurations[0], configurations[1])
-    ranksums(result, configurations[2], configurations[3])
-    ranksums(result, configurations[0], configurations[2])
-    ranksums(result, configurations[1], configurations[3])
-    ranksums(result, configurations[0], configurations[3])
-
     coverage(result)
 
-    out = "{} vs. {}: {:4.2f}% decrease (n = {}), {} vs. {} solved ({:+})"
+    out = "{} vs. {}: Sum of difference of logarithms is {:.2f}, {} vs. {} solved ({:+}). p = {:.5f}"
 
-    i, n, s1, s2 = do(result, '1|1.5', '0|1.5', label='f = 1.5')
-    print(out.format("f = 1.5: With", "without preprocessing", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
-    i, n, s1, s2 = do(result, '1|2', '0|2', label='f = 2')
-    print(out.format("f = 2:   With", "without preprocessing", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
-    plot('preprocessed', 'not preprocessed', 'p')
+    i, s1, s2, p = do(result, '1|1.5', '0|1.5', label='f = 1.5')
+    print(out.format("f = 1.5: With", "without preprocessing", i, s1, s2, s1 - s2, p))
+    i, s1, s2, p = do(result, '1|2', '0|2', label='f = 2')
+    print(out.format("f = 2:   With", "without preprocessing", i, s1, s2, s1 - s2, p))
+    plot('Preprocessed', 'Not preprocessed', 'p')
 
-    i, n, s1, s2 = do(result, '1|1.5', '1|2', label='preprocessed')
-    print(out.format("With    preprocessing: f = 1.5", "f = 2", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
-    i, n, s1, s2 = do(result, '0|1.5', '0|2', label='not preprocessed')
-    print(out.format("Without preprocessing: f = 1.5", "f = 2", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
+    i, s1, s2, p = do(result, '1|1.5', '1|2', label='preprocessed')
+    print(out.format("With    preprocessing: f = 1.5", "f = 2", i, s1, s2, s1 - s2, p))
+    i, s1, s2, p = do(result, '0|1.5', '0|2', label='not preprocessed')
+    print(out.format("Without preprocessing: f = 1.5", "f = 2", i, s1, s2, s1 - s2, p))
     plot('f = 1.5', 'f = 2', 'f')
 
-    i, n, s1, s2 = do(result, '1|1.5', '0|2', label='')
-    print(out.format("With optimizations", "without", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
+    i, s1, s2, p = do(result, '1|1.5', '0|2', label='')
+    print(out.format("With optimizations", "without", i, s1, s2, s1 - s2, p))
     next(cyc)
     plot('Optimized', 'Unoptimized', 'o')
 
     configurations = [0, 1, 2, 3]
 
-    i, n, s1, s2 = do(enc, 1, 0)
-    print(out.format("Sequential AMO (1)", "binomial AMO (0)", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
+    i, s1, s2, p = do(enc, 1, 0)
+    print(out.format("Sequential AMO (1)", "binomial AMO (0)", i, s1, s2, s1 - s2, p))
     next(cyc)
-    plot('sequential AMO', 'binomial AMO', 'amo', False)
+    plot('Sequential AMO', 'Binomial AMO', 'amo', False)
 
-    i, n, s1, s2 = do(enc, 2, 1)
-    print(out.format("With edge variables (2)", "without edge variables (1)", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
+    i, s1, s2, p = do(enc, 2, 1)
+    print(out.format("With edge variables (2)", "without edge variables (1)", i, s1, s2, s1 - s2, p))
     next(cyc)
-    plot('with edge variables', 'without edge variables', 'ev', False)
+    plot('With edge variables', 'Without edge variables', 'ev', False)
 
-    i, n, s1, s2 = do(enc, 3, 2)
-    print(out.format("With movement variables (3)", "without movement variables (2)", (1 - 1 / i) * 100, n, s1, s2, s1 - s2))
+    i, s1, s2, p = do(enc, 3, 2)
+    print(out.format("With movement variables (3)", "without movement variables (2)", i, s1, s2, s1 - s2, p))
     next(cyc)
-    plot('with movement variables', 'without movement variables', 'mv', False)
+    plot('With movement variables', 'Without movement variables', 'mv', False)
 
     for i, j in encodings:
         d = pd.concat(map(pd.read_csv, glob.glob(str(i) + '/*.csv')))
-        out = "Encoding {} needs {:9.1f} clauses, {:9.1f} literals and {:7.1f} variables on average"
+        out = "Encoding {} needs {:9.1f} clauses, {:9.1f} literals and {:8.1f} variables on average"
         print(out.format(j, d['n_clauses'].mean(), d['n_literals'].mean(), d['n_variables'].mean()))
