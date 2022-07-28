5e65e0b6b5017b58ccb7202e94b70a78a050ae9d
Pascal Bachor <bachor@cp-med.com>
2022-07-28 16:05:11 +0200

    Format evaluate.py

--- configuration/evaluate.py
+++ configuration/evaluate.py
@@ -2,85 +2,109 @@ import glob
 import math
 import pickle
 import pandas as pd
-import scipy.stats
 import itertools
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-def plot(xlabel: str, ylabel: str, filename: str, legend=True):
+
+def plot(xlabel: str, ylabel: str, filename: str):
     xpoints = ypoints = plt.xlim()
-    plt.plot(xpoints, ypoints, linestyle='-', zorder=1)
+    plt.plot(xpoints, ypoints, linestyle="-", zorder=1)
     axes = plt.gca()
     axes.yaxis.grid(True, zorder=0)
     axes.xaxis.grid(True, zorder=0)
-    plt.plot([1e3, 1e3], [0, 1e3], linestyle='-', color='salmon', zorder=2, lw=1)
-    plt.plot([0, 1e3], [1e3, 1e3], linestyle='-', color='salmon', zorder=2, lw=1)
-    plt.xscale('log')
-    plt.yscale('log')
+    plt.plot([1e3, 1e3], [0, 1e3], linestyle="-", color="salmon", zorder=2, lw=1)
+    plt.plot([0, 1e3], [1e3, 1e3], linestyle="-", color="salmon", zorder=2, lw=1)
+    plt.xscale("log")
+    plt.yscale("log")
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.axis('square')
+    plt.axis("square")
 
     f = axes.xaxis.get_major_formatter()
-    fx = lambda x, pos: '⊥' if pos == 5 else 1 if pos == 2 else 10 if pos == 3 else f.__call__(x, pos)
+    fx = (
+        lambda x, pos: "⊥"
+        if pos == 5
+        else 1
+        if pos == 2
+        else 10
+        if pos == 3
+        else f.__call__(x, pos)
+    )
     axes.xaxis.set_major_locator(ticker.LogLocator())
     axes.xaxis.set_major_formatter(ticker.FuncFormatter(fx))
 
     f = axes.yaxis.get_major_formatter()
-    fy = lambda y, pos: '⊥' if pos == 5 else 1 if pos == 2 else 10 if pos == 3 else f.__call__(y, pos)
+    fy = (
+        lambda y, pos: "⊥"
+        if pos == 5
+        else 1
+        if pos == 2
+        else 10
+        if pos == 3
+        else f.__call__(y, pos)
+    )
     axes.yaxis.set_major_locator(ticker.LogLocator())
     axes.yaxis.set_major_formatter(ticker.FuncFormatter(fy))
 
     if axes.get_legend_handles_labels()[0]:
         plt.legend(framealpha=1)
-    plt.savefig(filename, dpi=300, bbox_inches = 'tight')
+    plt.savefig(filename, dpi=300, bbox_inches="tight")
     print("Plot saved to file {}.png".format(filename))
     plt.close()
 
-params   = ['g', 'b', 'a', 'c']
-instance = params + ['seed']
-static   = instance + ['makespan']
 
-needed = static + ['config', 'result', 't_total']
+params = ["g", "b", "a", "c"]
+instance = params + ["seed"]
+static = instance + ["makespan"]
 
-pickle_jar = 'data.p'
+needed = static + ["config", "result", "t_total"]
+
+pickle_jar = "data.p"
 
 encodings = [(4, 0), (3, 1), (5, 2), (6, 3)]
 
-tail = '|600|4|1|1'
+tail = "|600|4|1|1"
+
 
 def load(encoding=False) -> dict:
 
     if encoding:
         ds = []
         for i, j in encodings:
-            d = pd.concat(map(pd.read_csv, glob.glob(str(i) + '/*.csv')))
-            d['config'] = j
+            d = pd.concat(map(pd.read_csv, glob.glob(str(i) + "/*.csv")))
+            d["config"] = j
             ds.append(d)
         df = pd.concat(ds)[needed]
     else:
-        df = pd.concat(map(pd.read_csv, glob.glob('[0-3]/*.csv')))[needed]
+        df = pd.concat(map(pd.read_csv, glob.glob("[0-3]/*.csv")))[needed]
 
-    cs = dict(tuple(df.groupby(['config'])))
+    cs = dict(tuple(df.groupby(["config"])))
 
     p = pd.DataFrame(columns=static)
     for c, d in cs.items():
-        p = p.merge(d[d['result'].isna()][static], on=static, how='outer')
-        assert(not any(p[instance].duplicated()))
+        p = p.merge(d[d["result"].isna()][static], on=static, how="outer")
+        assert not any(p[instance].duplicated())
 
     result = dict()
 
     for c, d in cs.items():
         ps = dict(tuple(d.groupby(params)))
         for i, s in ps.items():
-            s = s[s['result'].isna() | (s['result'] == 'Timeout')]
-            t = s.apply(lambda x: pd.NA if x['result'] == 'Timeout' else max(x['t_total'], 1), axis=1, result_type='reduce')
+            s = s[s["result"].isna() | (s["result"] == "Timeout")]
+            t = s.apply(
+                lambda x: pd.NA if x["result"] == "Timeout" else max(x["t_total"], 1),
+                axis=1,
+                result_type="reduce",
+            )
             result.setdefault(i, dict())
             result[i][c] = (t.mean(), t.std())
 
     return result
 
-cyc = itertools.cycle(['o', 'x'])
+
+cyc = itertools.cycle(["o", "x"])
+
 
 def do(data: dict, cf1, cf2, label=None):
 
@@ -105,9 +129,9 @@ def do(data: dict, cf1, cf2, label=None):
         if not pd.isna(v2):
             s2 += 1
 
-        if (pd.isna(v1)):
+        if pd.isna(v1):
             v1 = 1e6
-        if (pd.isna(v2)):
+        if pd.isna(v2):
             v2 = 1e6
 
         logs.append(math.log2(v2) - math.log2(v1))
@@ -122,18 +146,20 @@ def do(data: dict, cf1, cf2, label=None):
 
     return sum(logs), s1, s2
 
+
 def prepare(data: dict, cf):
     result = []
     for i, r in data.items():
         result.append(1e6 if pd.isna(r[cf][0]) else r[cf][0])
     return result
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     result = enc = None
 
     try:
-        with open(pickle_jar, 'rb') as fp:
+        with open(pickle_jar, "rb") as fp:
             print("Loading from jar")
             result, enc = pickle.load(fp)
     except FileNotFoundError:
@@ -141,51 +167,73 @@ if __name__ == '__main__':
         result = load()
         enc = load(True)
         print("Saving to jar")
-        with open(pickle_jar, 'wb') as fp:
+        with open(pickle_jar, "wb") as fp:
             pickle.dump([result, enc], fp)
 
-    plt.rcParams['axes.xmargin'] = 1e-2
-    plt.rcParams['axes.ymargin'] = 1e-2
+    plt.rcParams["axes.xmargin"] = 1e-2
+    plt.rcParams["axes.ymargin"] = 1e-2
 
-    configurations = list(map(lambda x: x + tail, ['0|2', '1|2', '0|1.5', '1|1.5']))
+    configurations = list(map(lambda x: x + tail, ["0|2", "1|2", "0|1.5", "1|1.5"]))
 
-    out = "{} vs. {}: Sum of difference of logarithms is {:.2f}, {} vs. {} solved ({:+})."
+    out = (
+        "{} vs. {}: Sum of difference of logarithms is {:.2f}, {} vs. {} solved ({:+})."
+    )
 
-    i, s1, s2 = do(result, '1|1.5', '0|1.5', label='f = 1.5')
+    i, s1, s2 = do(result, "1|1.5", "0|1.5", label="f = 1.5")
     print(out.format("f = 1.5: With", "without preprocessing", i, s1, s2, s1 - s2))
-    i, s1, s2 = do(result, '1|2', '0|2', label='f = 2')
+    i, s1, s2 = do(result, "1|2", "0|2", label="f = 2")
     print(out.format("f = 2:   With", "without preprocessing", i, s1, s2, s1 - s2))
-    plot('Preprocessed', 'Not preprocessed', 'p')
+    plot("Preprocessed", "Not preprocessed", "p")
 
-    i, s1, s2 = do(result, '1|1.5', '1|2', label='preprocessed')
+    i, s1, s2 = do(result, "1|1.5", "1|2", label="preprocessed")
     print(out.format("With    preprocessing: f = 1.5", "f = 2", i, s1, s2, s1 - s2))
-    i, s1, s2 = do(result, '0|1.5', '0|2', label='not preprocessed')
+    i, s1, s2 = do(result, "0|1.5", "0|2", label="not preprocessed")
     print(out.format("Without preprocessing: f = 1.5", "f = 2", i, s1, s2, s1 - s2))
-    plot('f = 1.5', 'f = 2', 'f')
+    plot("f = 1.5", "f = 2", "f")
 
-    i, s1, s2 = do(result, '1|1.5', '0|2', label='')
+    i, s1, s2 = do(result, "1|1.5", "0|2", label="")
     print(out.format("With optimizations", "without", i, s1, s2, s1 - s2))
     next(cyc)
-    plot('Optimized', 'Unoptimized', 'o')
+    plot("Optimized", "Unoptimized", "o")
 
     configurations = [0, 1, 2, 3]
 
     i, s1, s2 = do(enc, 1, 0)
     print(out.format("Sequential AMO (1)", "binomial AMO (0)", i, s1, s2, s1 - s2))
     next(cyc)
-    plot('Sequential AMO', 'Binomial AMO', 'amo', False)
+    plot("Sequential AMO", "Binomial AMO", "amo", False)
 
     i, s1, s2 = do(enc, 2, 1)
-    print(out.format("With edge variables (2)", "without edge variables (1)", i, s1, s2, s1 - s2))
+    print(
+        out.format(
+            "With edge variables (2)", "without edge variables (1)", i, s1, s2, s1 - s2
+        )
+    )
     next(cyc)
-    plot('With edge variables', 'Without edge variables', 'ev', False)
+    plot("With edge variables", "Without edge variables", "ev", False)
 
     i, s1, s2 = do(enc, 3, 2)
-    print(out.format("With movement variables (3)", "without movement variables (2)", i, s1, s2, s1 - s2))
+    print(
+        out.format(
+            "With movement variables (3)",
+            "without movement variables (2)",
+            i,
+            s1,
+            s2,
+            s1 - s2,
+        )
+    )
     next(cyc)
-    plot('With movement variables', 'Without movement variables', 'mv', False)
+    plot("With movement variables", "Without movement variables", "mv", False)
 
     for i, j in encodings:
-        d = pd.concat(map(pd.read_csv, glob.glob(str(i) + '/*.csv')))
+        d = pd.concat(map(pd.read_csv, glob.glob(str(i) + "/*.csv")))
         out = "Encoding {} needs {:9.1f} clauses, {:9.1f} literals and {:8.1f} variables on average"
-        print(out.format(j, d['n_clauses'].mean(), d['n_literals'].mean(), d['n_variables'].mean()))
+        print(
+            out.format(
+                j,
+                d["n_clauses"].mean(),
+                d["n_literals"].mean(),
+                d["n_variables"].mean(),
+            )
+        )
