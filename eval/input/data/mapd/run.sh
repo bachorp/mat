1b20de95f3794825614fc185b99f6f8b80c0a890
Pascal Bachor <bachor@cp-med.com>
2022-12-28 22:43:19 +0100

    Move stuff

--- eval/input/data/mapd/run.sh
+++ eval/input/data/mapd/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #SBATCH -a 0-199
 #SBATCH -p gki_cpu-cascadelake
 #SBATCH --cpus-per-task=4
@@ -9,5 +9,5 @@ let "SEED = SLURM_ARRAY_TASK_ID / 10"
 let "GRID = SLURM_ARRAY_TASK_ID % 10 + 3"
 
 mkdir -p $GRID
-/usr/bin/time --verbose ../../build/mat f grid $GRID seed $SEED $GRID/$SEED.csv
+/usr/bin/time --verbose ../build/mat d grid $GRID seed $SEED $GRID/$SEED.csv
 exit $?
